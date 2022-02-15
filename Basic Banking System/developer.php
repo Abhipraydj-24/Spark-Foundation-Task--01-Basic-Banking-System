939430a20fa29ebc0eaf7485aae5d78db51667cc
Abhipray D. jawanjal <94818317+Abhipraydj-24@users.noreply.github.com>
2022-02-15 14:23:40 +0530

    Add files via upload

---
+++ Basic Banking System/developer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abhipray Jawanjal</title>
+    <link rel="stylesheet" href="Style/developer.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="apple-touch-icon" sizes="180x180" href="Images/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="Images/abhi-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="Images/abhi-16x16.png">
+</head>
+<body data-theme="">
+   
+    <div class="card">
+        <img src="Images/abhi.jpg" alt="">
+        <h2>Abhipray Jawanjal</h2>
+        <p>Computer Engineer</p>
+        <div class="social">
+            <a class="social-link" href="https://github.com/abhipraydj24"><img src="Images/icons8-github.svg" alt=""></a>
+            <a class="social-link" href="https://www.instagram.com/invites/contact/?i=16c4m7x94pgwa&utm_content=5j6buzj"><img src="Images/instagram4.svg" alt=""></a>
+            <a class="social-link" href="https://www.linkedin.com/in/abhiprayjawanjal"><img src="Images/icons8-linkedin.svg" alt=""></a>
+          
+          </div>
+    </div>
+
+   
+</body>
+</html>
